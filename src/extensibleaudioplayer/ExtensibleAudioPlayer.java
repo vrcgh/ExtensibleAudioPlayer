5d94c4895389585d23bf81dd30be53812ee24233
Matthias <17014025@uhi.ac.uk>
2021-07-04 09:41:42 +0100

    initial commit

---
+++ src/extensibleaudioplayer/ExtensibleAudioPlayer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package extensibleaudioplayer;
+
+/**
+ *
+ * @author 17014025@uhi.ac.uk
+ */
+public class ExtensibleAudioPlayer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
